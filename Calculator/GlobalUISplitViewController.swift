4f0a8ea2218c22738b11f010a467c36a972c1587
jgreenb2 <jgreenb2@ford.com>
2015-06-17 13:47:39 -0400

    added UISplitViewController code to force the master to show rather than the detail on an iPhone

---
+++ Calculator/GlobalUISplitViewController.swift
@@ -0,0 +1,25 @@
+//
+//  GlobalUISplitViewController.swift
+//  Calculator
+//
+//  Created by jeff greenberg on 6/17/15.
+//  Copyright (c) 2015 Jeff Greenberg. All rights reserved.
+//
+//
+// forces the split view to show the master when collapsed at startup
+//
+import UIKit
+
+class GlobalUISplitViewController: UISplitViewController, UISplitViewControllerDelegate {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.delegate = self
+    }
+    
+    func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController: UIViewController!, ontoPrimaryViewController primaryViewController: UIViewController!) -> Bool{
+        return true
+    }
+    
+}
