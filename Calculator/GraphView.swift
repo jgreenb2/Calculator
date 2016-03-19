8baa5666f40e1caeb3c60493a8fc06f6aee89e70
jgreenb2 <jgreenb@mac.com>
2016-03-18 20:49:12 -0400

    well i think we've finally got something workable

--- Calculator/GraphView.swift
+++ Calculator/GraphView.swift
@@ -129,10 +129,6 @@ class GraphView: UIView {
         return CGPoint(x: XToScreen(x), y: YToScreen(y))
     }
     
-    func XToScreen(x: Double) -> CGFloat {
-        return CGFloat(x)*density.x + graphCenter.x
-    }
-    
     func ScreenToX(i: CGFloat) -> Double {
         return Double((i - graphCenter.x)/density.x)
     }
@@ -141,8 +137,16 @@ class GraphView: UIView {
         return Double((graphCenter.y - i)/density.y)
     }
     
+    func XToScreen(x: Double) -> CGFloat {
+        return CGFloat(x)*density.x + graphCenter.x
+    }
+    
     func YToScreen(y: Double) -> CGFloat {
         return -CGFloat(y)*density.y + graphCenter.y
+    }
+    
+    func ScreenToXY(p: CGPoint) -> CGPoint {
+        return CGPoint(x: ScreenToX(p.x), y: ScreenToY(p.y))
     }
     
     // gesture handling functions
@@ -213,12 +217,14 @@ class GraphView: UIView {
                 // compute the center of the pinch
                 let deltaTouch = touch2 - touch1
                 touchCenter = touch1 + deltaTouch/2.0
+                
                 //let touchCenterInGraphCoord = CGPoint(x: ScreenToX(touchCenter.x), y: ScreenToY(touchCenter.y))
                 
                 // now compute the amount the origin has to move to keep this point 
                 // in the same position on the screen
                 //let translation = CGPoint(x: touchCenterInGraphCoord.x*densityX*(1.0-1.0/scalex), y: touchCenterInGraphCoord.y*densityY*(1.0-1.0/scaley))
-                let translation = CGPoint(x: (touchCenter.x-graphCenter.x)*(1.0-1.0/scalex), y: (graphCenter.y-touchCenter.y)*(1.0-1.0/scaley))
+                //let translation = CGPoint(x: (touchCenter.x-graphCenter.x)*(1.0-1.0/scalex), y: (graphCenter.y-touchCenter.y)*(1.0-1.0/scaley))
+                let translation = CGPoint(x: (touchCenter.x-graphCenter.x)*(1.0-scalex), y: (touchCenter.y-graphCenter.y)*(1.0-scaley))
                 graphOrigin = graphCenter + translation
                 
                 gesture.scale=1
