b9938ece25412bc0fe8f1daa0fd12fd54fd57b17
jgreenb2 <jgreenb@mac.com>
2016-05-11 20:05:21 -0400

    a little fix to the button guard code -- should be ok now even if no delegate is set

--- CalculatorButton.swift
+++ CalculatorButton.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Jeff Greenberg. All rights reserved.
 //
 
-// class exists so that the calculator can inspect button all button events
+// class exists so that the calculator can inspect all button events
 
 protocol CalcEntryMode {
     func setEntryModeNormal()
@@ -20,10 +20,10 @@ class CalculatorButton: UIButton {
     var delegate:CalcEntryMode?
     
     // catch all actions make sure that only a digit key can
-    // be pressed in format mode. 
+    // be pressed in format mode.
     override func sendAction(action: Selector, to target: AnyObject?, forEvent event: UIEvent?) {
         guard (self is CalculatorDigits) || (delegate?.isEntryModeNormal())! else {
-            delegate!.setEntryModeNormal()
+            delegate?.setEntryModeNormal()
             return
         }
         super.sendAction(action, to: target, forEvent: event)
